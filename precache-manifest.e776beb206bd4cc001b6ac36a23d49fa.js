73029e021f66088c48288a9f53027506d2c63f9a
Hyukjin Kwon <hjnim0909@gmail.com>
2020-07-06 16:08:17 +0900

    Updates

---
+++ precache-manifest.e776beb206bd4cc001b6ac36a23d49fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b6e04b41061e19fe31c2d52c528635",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a508221bec217625fde0",
+    "url": "/movie_app_2020/static/css/main.e9d2b8ee.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a508221bec217625fde0",
+    "url": "/movie_app_2020/static/js/main.175cea04.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
